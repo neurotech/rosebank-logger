b7f11adaa9edf3e1984cd15682e629de84e74677
Tim Douglas <neurotech@gmail.com>
2015-11-11 14:06:59 +1100

    Merged setup.js and app.js
    If database doesn’t exist, create the schema and then proceed to start
    the server.

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 'use strict';
 
+var _ = require('lodash');
+var async = require('async');
+var config = require('./config');
 var server = require('./lib/hapi');
+var r = require('./lib/db');
+require('rethinkdb-init')(r);
 
-server.start(function () {});
+var schema = ['fatal', 'error', 'warn', 'info'];
+var check = _.includes(r.dbList(), config.rethinkdb.db);
+
+if (check === false) {
+  async.series([
+    function skeleton (callback) {
+      r.init({
+          host: config.rethinkdb.servers[0].host,
+          port: config.rethinkdb.servers[0].port,
+          db: config.rethinkdb.db
+        }, schema)
+        .then(function (conn) {
+          console.log('[rethinkdb] ' + 'Created db: ' + config.rethinkdb.db + ' with tables: ' + schema);
+          callback(null, 'skeleton');
+        }
+      );
+    },
+    function complete (callback) {
+      r.getPoolMaster().drain();
+      console.log('[rethinkdb] ' + 'Complete - drained connection pool.');
+      server.start(function () {
+        console.log('rosebank-logger listening at http://' + config.http.host + ':' + config.http.port + '/logger');
+      });
+      callback(null, 'complete');
+    }
+  ]);
+}
+
+console.log('DB setup. Starting server...');
+server.start(function () {
+  console.log('rosebank-logger listening at http://' + config.http.host + ':' + config.http.port + '/logger');
+});
